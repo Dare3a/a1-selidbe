35e27216b5d97ee7776a12ace41ebf2a3acedebe
darko2019 <darko.ilic83@gmail.com>
2024-01-27 19:06:54 +0100

    Ubacen php.mailer i podesen toast za poslatu poruku i reset kontakt forme

--- scripts/main.js
+++ scripts/main.js
@@ -54,3 +54,28 @@ if (padajuciMeni) {
         })
     })
 }
+
+// Toast za poslatu poruku u kontakt formi
+const toastMsg = document.querySelector('.toast')
+
+function toast() {
+    toastMsg.classList.replace('hide', 'show')
+    setTimeout(() => {
+        toastMsg.classList.replace('show', 'hide')
+
+    }, 2000);
+}
+
+// Reset kontakt forme i modal kontakt forme
+const kontaktForma = document.querySelector("#form-contact")
+const kontaktFormaBtn = document.querySelector('.send-dugme')
+if (kontaktForma) {
+    kontaktForma.addEventListener('submit', toast)
+    kontaktForma.addEventListener('submit', resetForm)
+}
+
+function resetForm() {
+    setTimeout(() => {
+        kontaktForma.reset();
+    }, 1000);
+}
